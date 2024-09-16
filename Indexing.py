efcb4e4e08a87b7f45fb98a1b25e98e2c20ae908
IBRAHIM ALOBAID <ibrahim.iaob@gmail.com>
2024-09-17 00:35:52 +0100

    replaceing Dict with Trie(DIDNT WORK WELL)

--- Indexing.py
+++ Indexing.py
@@ -1,14 +1,15 @@
 from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+import pygtrie
 import time , datetime
 import json
-def create_index(key:int , Document:str , InvertedIndexDictionary:dict):
+def create_index(key:int , Document:str , InvertedIndexTrie : pygtrie.CharTrie):
     Tokens = tokenizer.tokenize(Document)
     for Token in Tokens: 
-        if InvertedIndexDictionary.get(Token) == None:
-            InvertedIndexDictionary[Token] = [key]
+        if not InvertedIndexTrie.has_key(Token):
+            InvertedIndexTrie[Token] = [key]
         else:
-            insert_To_sorted_list_and_keep_sorted(InvertedIndexDictionary[Token] , key)
+            insert_To_sorted_list_and_keep_sorted(InvertedIndexTrie[Token] , key)
             
 
 def insert_To_sorted_list_and_keep_sorted(LIST:list, key:int):
@@ -18,7 +19,7 @@ def insert_To_sorted_list_and_keep_sorted(LIST:list, key:int):
             return
     LIST.insert(len(LIST), key)
     
-inverted_index = {}     
+InvertedIndexTrie = pygtrie.CharTrie()    
     
 
 
@@ -27,11 +28,11 @@ with open("data.json" , "r") as f:
     
 for key  , document in data.items():
     if int(key) % 1000 == 0 :
-        print(f"{key} : {datetime.datetime.now()}")
-    create_index(key , document , inverted_index)
+        print(f"{key} : {datetime.datetime.minute()}")
+    create_index(key , document , InvertedIndexTrie)
     
 with open("inverted_index.json" , "w") as f : 
-    json.dump(inverted_index , f , indent=0)
+    json.dump(InvertedIndexTrie , f , indent=0)
     
     
             
